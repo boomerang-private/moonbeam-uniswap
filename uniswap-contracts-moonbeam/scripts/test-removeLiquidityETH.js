aafaf9afab622938acd7ab931ae1fe0a4e517703
frank2025888 <fran2025888@gmail.com>
2025-02-25 17:06:27 +0800

    update address

--- uniswap-contracts-moonbeam/scripts/test-removeLiquidityETH.js
+++ uniswap-contracts-moonbeam/scripts/test-removeLiquidityETH.js
@@ -6,10 +6,15 @@ async function deploy() {
    callerAddress = account.address;
    console.log(`call contracts using ${callerAddress}`);
 
-   const router = '0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108';
-   const token = '0xa2dae3ed07ea2d595b6dfa6370772e23d508068b';
-   const lpToken = '0x1f033f266d40df38a053df1ea87b92b9310c9443';
-   const pool = '0xd8fd0e0051482a947a7d55659d614a8354682923';
+   // dev
+   //const router = '0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108';
+   //const token = '0xa2dae3ed07ea2d595b6dfa6370772e23d508068b';
+   //const lpToken = '0x1f033f266d40df38a053df1ea87b92b9310c9443';
+
+   // mandala
+   const router = '0x1f11F9df8e95dBc007f2A7Ec8E4D889810aA0A60';
+   const token = '0x6A4EbA793c6cc0D9167fbE03B944633B8098531B';
+   const lpToken = '0x2B4e2B09593b0583703CdcaBe254BA1A937Cc9B9';
 
    const tokenFactory = await ethers.getContractFactory('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20');
    const t = tokenFactory.attach(lpToken);
--- uniswap-contracts-moonbeam/scripts/test-swapExactTokensForETH.js
+++ uniswap-contracts-moonbeam/scripts/test-swapExactTokensForETH.js
@@ -6,9 +6,15 @@ async function deploy() {
    callerAddress = account.address;
    console.log(`call contracts using ${callerAddress}`);
 
-   const router = '0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108';
-   const weth = '0x3d3593927228553b349767ABa68d4fb1514678CB';
-   const token = 'a2dae3ed07ea2d595b6dfa6370772e23d508068b';
+   // dev
+   //const router = '0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108';
+   //const weth = '0x3d3593927228553b349767ABa68d4fb1514678CB';
+   //const token = 'a2dae3ed07ea2d595b6dfa6370772e23d508068b';
+
+   // mandala
+   const router = '0x1f11F9df8e95dBc007f2A7Ec8E4D889810aA0A60';
+   const weth = '0x83112596f4aE4c874a549B2C9EABBecbfA2eE097';
+   const token = '0x6A4EbA793c6cc0D9167fbE03B944633B8098531B';
 
    const tokenFactory = await ethers.getContractFactory('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20');
    const t = tokenFactory.attach(token);
