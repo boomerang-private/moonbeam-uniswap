fbae32c5efb72024238e79eae1cbfebb5482b9f9
frank2025888 <fran2025888@gmail.com>
2025-02-25 17:53:54 +0800

    update address

--- uniswap-contracts-moonbeam/scripts/test-removeLiquidityETH.js
+++ uniswap-contracts-moonbeam/scripts/test-removeLiquidityETH.js
@@ -1,4 +1,5 @@
 const { ethers } = require('hardhat');
+const { ADDRESSES } = require('./addresses');
 
 // Deploy function
 async function deploy() {
@@ -6,31 +7,24 @@ async function deploy() {
    callerAddress = account.address;
    console.log(`call contracts using ${callerAddress}`);
 
-   // dev
-   //const router = '0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108';
-   //const token = '0xa2dae3ed07ea2d595b6dfa6370772e23d508068b';
-   //const lpToken = '0x1f033f266d40df38a053df1ea87b92b9310c9443';
-
-   // mandala
-   const router = '0x1f11F9df8e95dBc007f2A7Ec8E4D889810aA0A60';
-   const token = '0x6A4EbA793c6cc0D9167fbE03B944633B8098531B';
-   const lpToken = '0x2B4e2B09593b0583703CdcaBe254BA1A937Cc9B9';
+   const network = hre.network.name;
+   const addresses = ADDRESSES[network];
 
    const tokenFactory = await ethers.getContractFactory('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20');
-   const t = tokenFactory.attach(lpToken);
+   const t = tokenFactory.attach(addresses.lpToken);
    const tx1 = await t.approve(
-      router, // spender
+      addresses.router, // spender
       ethers.constants.MaxUint256, // value
    );
    console.log(`lp token approve result: `, tx1);
 
    const routerFactory = await ethers.getContractFactory('UniswapV2Router02');
-   const r = routerFactory.attach(router);
+   const r = routerFactory.attach(addresses.router);
 
    const currentTimestamp = Math.floor(Date.now() / 1000);
    const deadline = currentTimestamp + 20 * 60; // 20 mins
    const tx2 = await r.removeLiquidityETH(
-      token, // token
+      addresses.token, // token
       ethers.utils.parseEther("30"), // liquidity
       ethers.utils.parseEther("0"), // amountTokenMin
       ethers.utils.parseEther("0"), // amountETHMin
--- uniswap-contracts-moonbeam/scripts/test-swapExactTokensForETH.js
+++ uniswap-contracts-moonbeam/scripts/test-swapExactTokensForETH.js
@@ -1,4 +1,5 @@
 const { ethers } = require('hardhat');
+const { ADDRESSES } = require("./addresses");
 
 // Deploy function
 async function deploy() {
@@ -6,33 +7,26 @@ async function deploy() {
    callerAddress = account.address;
    console.log(`call contracts using ${callerAddress}`);
 
-   // dev
-   //const router = '0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108';
-   //const weth = '0x3d3593927228553b349767ABa68d4fb1514678CB';
-   //const token = 'a2dae3ed07ea2d595b6dfa6370772e23d508068b';
-
-   // mandala
-   const router = '0x1f11F9df8e95dBc007f2A7Ec8E4D889810aA0A60';
-   const weth = '0x83112596f4aE4c874a549B2C9EABBecbfA2eE097';
-   const token = '0x6A4EbA793c6cc0D9167fbE03B944633B8098531B';
+   const network = hre.network.name;
+   const addresses = ADDRESSES[network];
 
    const tokenFactory = await ethers.getContractFactory('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20');
-   const t = tokenFactory.attach(token);
+   const t = tokenFactory.attach(addresses.token);
    const tx1 = await t.approve(
-      router, // spender
+      addresses.router, // spender
       ethers.constants.MaxUint256, // value
    );
    console.log(`token approve result: `, tx1);
 
    const routerFactory = await ethers.getContractFactory('UniswapV2Router02');
-   const r = routerFactory.attach(router);
+   const r = routerFactory.attach(addresses.router);
 
    const currentTimestamp = Math.floor(Date.now() / 1000);
    const deadline = currentTimestamp + 20 * 60; // 20 mins
    const tx2 = await r.swapExactTokensForETH(
       ethers.utils.parseEther("2000"), // amountIn
       ethers.utils.parseEther("0.1"), // amountOutMin
-      [token, weth], // path
+      [addresses.token, addresses.waca], // path
       callerAddress, // to
       deadline, // deadline
    );
